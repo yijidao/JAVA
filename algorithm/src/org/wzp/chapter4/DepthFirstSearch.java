4d21fa69fdbf71fe0831a4e256b2399018853d09
yijidao <2815451505@qq.com>
2018-03-20 23:58:21 +0800

    DepthFirstSearch 增加路径及相关方法

--- algorithm/src/org/wzp/chapter4/DepthFirstSearch.java
+++ algorithm/src/org/wzp/chapter4/DepthFirstSearch.java
@@ -1,4 +1,7 @@
 package org.wzp.chapter4;
+
+import org.wzp.chapter1.NodeStack;
+
 /**
  * 深度优先搜索
  * @author wzp
@@ -6,11 +9,16 @@ package org.wzp.chapter4;
  *
  */
 public class DepthFirstSearch {
-	private boolean[] marked;
-	private int count;
+	private boolean[] marked; // 与 s 是否连通
+	private int count; // 与s连通的顶点总数
+	private int[] edgeTo; // 从起点到一个顶点的已知路径上的最后一个顶点
+	private final int s; // 起点
+	
 	
 	public DepthFirstSearch(Graph G, int s) {
 		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		this.s = s;
 		dfs(G, s);
 	}
 	
@@ -24,8 +32,10 @@ public class DepthFirstSearch {
 		marked[v] = true;
 		count++;
 		for(int w : G.adj(v))
-			if(!marked[w])
+			if(!marked[w]) {
+				edgeTo[w] = v;
 				dfs(G, w);
+			}
 	}
 	
 	/**
@@ -43,5 +53,29 @@ public class DepthFirstSearch {
 	 */
 	public int count() {
 		return count;
+	}
+	
+	/**
+	 * 是否存在 s 到 v 的路径
+	 * @param v
+	 * @return
+	 */
+	public boolean hasPathTo(int v) {
+		return marked[v];
+	}
+	
+	/**
+	 * 返回 s 到 v 的路径，如果不存在，返回 null
+	 * @param v
+	 * @return
+	 */
+	public Iterable<Integer> pathTo(int v){
+		if(!hasPathTo(v))
+			return null;
+		NodeStack<Integer> path = new NodeStack<Integer>();
+		for(int x= v; x != s; x = edgeTo[x])
+			path.push(x);
+		path.push(s);
+		return path;
 	}
 }
